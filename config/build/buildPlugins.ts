d507c420a965da2cf45695b60acca7c3dbaa4f80
Nikita Lavrentev <n.lawrentiew2017@yandex.ru>
2023-02-18 20:13:49 +0600

    added ReactRefreshWebpackPlugin HotModuleReplacementPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,10 +2,11 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -18,4 +19,11 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
             __IS_DEV__: JSON.stringify(isDev)
         })
     ]
+
+    if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin())
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+    }
+
+    return plugins
 }
--- src/pages/About/ui/About.tsx
+++ src/pages/About/ui/About.tsx
@@ -3,6 +3,7 @@ import {useTranslation} from "react-i18next";
 
 const About = () => {
     const {t} = useTranslation("about")
+
     return (
         <div>
             {t("label.about")}
