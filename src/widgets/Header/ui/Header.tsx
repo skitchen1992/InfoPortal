338fa2ecda1dac6ae5d6ba4b1652dfc3f187fab3
Nikita Lavrentev <n.lawrentiew2017@yandex.ru>
2023-03-12 19:48:42 +0600

    update sidebar

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,10 +1,7 @@
 import React, { FC } from 'react';
-import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink } from 'shared/ui/AppLink/AppLink';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
-import { useTranslation } from 'react-i18next';
 import cls from './Header.module.scss';
 
 interface IProps {
@@ -13,18 +10,9 @@ interface IProps {
 
 export const Header: FC<IProps> = (props) => {
     const { className } = props;
-    const { t } = useTranslation();
 
     return (
         <div className={classNames(cls.root, {}, [className])}>
-            <div className={classNames(cls.links, {}, [])}>
-                <AppLink to={RoutePath.main}>
-                    {t('page.main')}
-                </AppLink>
-                <AppLink to={RoutePath.about}>
-                    {t('page.about')}
-                </AppLink>
-            </div>
             <div className={cls.actions}>
                 <ThemeSwitcher />
                 <LangSwitcher />
--- src/widgets/SideBar/ui/Sidebar/SideBar.tsx
+++ src/widgets/SideBar/ui/Sidebar/SideBar.tsx
@@ -1,7 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { FC, useState } from 'react';
-import { List } from 'phosphor-react';
+import React, { FC, useState } from 'react';
+import { House, List, ListDashes } from 'phosphor-react';
 import { IconButton } from 'shared/ui/IconButton/IconButton';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { useTranslation } from 'react-i18next';
 import cls from './SideBar.module.scss';
 
 interface IProps {
@@ -13,6 +16,8 @@ export const SideBar: FC<IProps> = (props) => {
 
     const [collapsed, setCollapsed] = useState(false);
 
+    const { t } = useTranslation();
+
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
@@ -21,6 +26,28 @@ export const SideBar: FC<IProps> = (props) => {
         <div data-testid="SideBar" className={classNames(cls.root, { [cls.collapsed]: collapsed }, [className])}>
             <div className={classNames(cls.wrapBtn, { [cls.collapsed]: collapsed }, [className])}>
                 <IconButton data-testid="SideBarButton" onClick={onToggle}><List /></IconButton>
+
+                <div className={classNames(cls.links, {}, [])}>
+
+                    <AppLink to={RoutePath.main}>
+                        <div className={classNames(cls.link, {}, [])}>
+                            <House size={26} />
+                            <div className={classNames(cls.label, { [cls.collapsed]: collapsed }, [])}>
+                                {t('page.main')}
+                            </div>
+                        </div>
+                    </AppLink>
+
+                    <AppLink to={RoutePath.about}>
+                        <div className={classNames(cls.link, {}, [])}>
+                            <ListDashes size={26} />
+                            <div className={classNames(cls.label, { [cls.collapsed]: collapsed }, [])}>
+                                {t('page.about')}
+                            </div>
+                        </div>
+                    </AppLink>
+
+                </div>
             </div>
         </div>
     );
