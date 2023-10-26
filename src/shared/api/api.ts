4f1abc1165c9a39613e0742fff879c60042c8418
nikita <nikita.lavrentyev@mysky.com>
2023-10-26 18:49:10 +0400

    Set up request interceptor to add authorization header
    Refactored the way the authorization header is added to the API calls. Instead of setting it directly in the API instance, a new request interceptor is introduced. The interceptor is designed to check if headers are present in the configuration, then add the 'authorization' field picked from local storage. This ensures that the latest token is always used for authenticated requests.

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,14 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/consts/localStorage';
 
 export const API = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-    },
+});
+
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+
+    if (config.headers) {
+        config.headers.authorization = token;
+    }
+
+    return config;
 });
